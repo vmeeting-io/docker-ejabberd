f9a9aecc5e1244220f85690f53773ec83b4f58cc
TaeHwan Eun <theun@postech.ac.kr>
2021-07-09 03:29:13 +0000

    add XMPP_MUC_DOMAIN to ejabberd.yml add speakerstats module

--- ecs/rootfs/entrypoint.sh
+++ ecs/rootfs/entrypoint.sh
@@ -23,10 +23,13 @@ function register_jitsi_users {
 
 
 JWT_SECRET=$(echo -n "$JWT_APP_SECRET" | base64 | tr -d '\n')
+XMPP_MUC_DOMAIN_PREFIX=$(echo -n "$XMPP_MUC_DOMAIN" | sed '/\..*//')
 
 # substitute variables in ejabberd.yml
 sed -i \
     -e "s/{XMPP_DOMAIN}/$XMPP_DOMAIN/g" \
+    -e "s/{XMPP_MUC_DOMAIN}/$XMPP_MUC_DOMAIN/g" \
+    -e "s/{XMPP_MUC_DOMAIN_PREFIX}/$XMPP_MUC_DOMAIN_PREFIX/g" \
     -e "s/{JWT_SECRET}/$JWT_SECRET/g" \
     -e "s/{JICOFO_COMPONENT_SECRET}/$JICOFO_COMPONENT_SECRET/g" \
     -e "s/{DEFAULT_SITE_ID}/$DEFAULT_SITE_ID/g" \
